41749d53875cae579f274971da942fc86339d0e1
SangNguyen-Projects <76726764+SangNguyen-Projects@users.noreply.github.com>
2023-12-03 13:37:50 -0800

    Delete src directory

--- src/LifeApp.java
+++
@@ -1,9 +0,0 @@
-public class LifeApp {
-    public static void main(String[] args) {
-        /*
-        The application instantiates a model.
-        It then instantiates a view, passing it a reference to the model.
-        Finally, it tells the view to run()
-         */
-    }
-}
--- src/LifeGUIView.java
+++
@@ -1,6 +0,0 @@
-/*
-Do this last!
-Create a nice GUI-based GoL, using DrawingPanel and what you've learned from the GUI-based TTT project :)
- */
-public class LifeGUIView {
-}
